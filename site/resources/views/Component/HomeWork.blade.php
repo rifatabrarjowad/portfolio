3aac9b24c662c417167d719e89b9f5afc2086532
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-10-10 20:02:40 +0600

    work button

--- site/resources/views/Component/HomeWork.blade.php
+++ site/resources/views/Component/HomeWork.blade.php
@@ -15,13 +15,13 @@
                 <div class="card">
                     <img src="./img/photo.jpg" class="card-img-top" alt="..." width="100%" height="300px">
                     <div class="card-body">
-                        <h4 class="card-title" style=" color: #200959;">Card title</h4>
+                        <h2 class="card-title" style=" color:#ff8206;">Blog Site</h2>
                         <p class="card-text" style=" color: black;">
                             This is a longer card with supporting text below as a natural
                             lead-in to additional content. This content is a little bit
                             longer.
                         </p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <a href="#" class="btn get-project-link">Project Link</a>
                     </div>
                 </div>
             </div>
