1d44ca739f3e2dec383b1b0038f408d771ee86c9
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-10-10 21:20:20 +0600

    swork

---
+++ site/resources/views/Component/HomeSocialWork.blade.php
@@ -0,0 +1,58 @@
+<section id="socialWork">
+    <div class="container">
+        <div class="row">
+            <h1 style="
+                color: white;
+                text-align: center;
+                font-size: 50px;
+                font-weight: bold;
+                text-decoration: underline;
+                text-decoration-color: #ff8206;
+              ">
+                Social Work
+            </h1>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="./img/photo.jpg" class="card-img-top" alt="..." width="100%" height="300px">
+                    <div class="card-body">
+                        <h2 class="card-title" style=" color:#ff8206;">Blog Site</h2>
+                        <p class="card-text" style=" color: black;">
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                        <a href="#" class="btn get-project-link">Project Link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="./img/photo.jpg" class="card-img-top" alt="..." width="100%" height="300px">
+                    <div class="card-body text-center">
+                        <h2 class="card-title" style=" color:#ff8206;">Blog Site</h2>
+                        <p class="card-text" style=" color: black;">
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                        <a href="#" class="btn get-project-link">Project Link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="./img/photo.jpg" class="card-img-top" alt="..." width="100%" height="300px">
+                    <div class="card-body text-center">
+                        <h2 class="card-title " style=" color:#ff8206;">Blog Site</h2>
+                        <p class="card-text text-start" style=" color: black;">
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                        <a href="#" class="btn get-project-link">Project Link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
