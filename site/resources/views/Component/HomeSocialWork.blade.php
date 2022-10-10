e15d4be1151d2896c04760e600e8309e8f7f3416
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-10-11 02:03:59 +0600

    swork

--- site/resources/views/Component/HomeSocialWork.blade.php
+++ site/resources/views/Component/HomeSocialWork.blade.php
@@ -2,6 +2,16 @@
     <div class="container">
         <!-- Gallery -->
         <div class="row">
+            <h1 style="
+                color: white;
+                text-align: center;
+                font-size: 50px;
+                font-weight: bold;
+                text-decoration: underline;
+                text-decoration-color: #ff8206;
+              ">
+                Community Work
+            </h1>
             <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
                 <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
                     class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
