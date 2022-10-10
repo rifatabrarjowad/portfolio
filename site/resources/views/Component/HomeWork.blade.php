eec51935af4a3b558cd18e10982c8e7de0534277
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-10-10 20:10:33 +0600

    update

--- site/resources/views/Component/HomeWork.blade.php
+++ site/resources/views/Component/HomeWork.blade.php
@@ -28,28 +28,28 @@
             <div class="col-md-4">
                 <div class="card">
                     <img src="./img/photo.jpg" class="card-img-top" alt="..." width="100%" height="300px">
-                    <div class="card-body">
-                        <h4 class="card-title" style=" color: #200959;">Card title</h4>
+                    <div class="card-body text-center">
+                        <h2 class="card-title" style=" color:#ff8206;">Blog Site</h2>
                         <p class="card-text" style=" color: black;">
                             This is a longer card with supporting text below as a natural
                             lead-in to additional content. This content is a little bit
                             longer.
                         </p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <a href="#" class="btn get-project-link">Project Link</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card">
                     <img src="./img/photo.jpg" class="card-img-top" alt="..." width="100%" height="300px">
-                    <div class="card-body">
-                        <h4 class="card-title" style=" color: #200959;">Card title</h4>
-                        <p class="card-text" style=" color: black;">
+                    <div class="card-body text-center">
+                        <h2 class="card-title " style=" color:#ff8206;">Blog Site</h2>
+                        <p class="card-text text-start" style=" color: black;">
                             This is a longer card with supporting text below as a natural
                             lead-in to additional content. This content is a little bit
                             longer.
                         </p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <a href="#" class="btn get-project-link">Project Link</a>
                     </div>
                 </div>
             </div>
